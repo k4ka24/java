6e62bdfbdc8956340b10fac70580df920a766e9f
k4ka24 <kakaefendi4@gmail.com>
2025-01-22 11:30:40 +0700

    Add files via upload

---
+++ Folder Baru/belajar.java/pasien.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class pasien {
+    public static void main(String[] args) {
+        //Mendata Pasien
+        int id_pasien;
+        String nama,alamat,keluhan;
+        Scanner Input = new Scanner(System.in);
+        
+    }
+
+}
